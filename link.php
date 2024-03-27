e9c3d2a2e390ea6b28b55f779f3f503293ddd6bc
LanceTheWeakProgrammer <153787155+LanceTheWeakProgrammer@users.noreply.github.com>
2024-03-27 11:26:40 +0800

    Add files via upload

---
+++ link.php
@@ -0,0 +1,16 @@
+<!-- Bootstrap 5 -->
+<link rel="stylesheet" href="../lib/bootstrap@5.3.2/css/bootstrap.min.css">
+<script src="../lib/bootstrap@5.3.2/js/bootstrap.bundle.min.js"></script>
+
+<!-- Bootstrap Icon -->
+<link rel="stylesheet" href="../lib/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+
+<!-- Vue.js -->
+<script src="../lib/vue@3.3.9/vue.global.js"></script>
+
+
+<!-- Fonts -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Merienda:wght@400;700&family=Poppins:ital,wght@0,400;0,500;1,600&display=swap" rel="stylesheet">
